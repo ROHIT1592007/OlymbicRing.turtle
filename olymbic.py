93a00189d1eb713aa421b2baff5c978b1f246a2e
K.V.ROHIT <87985502+ROHIT1592007@users.noreply.github.com>
2021-12-28 12:41:01 +0530

    Add files via upload

---
+++ olymbic.py
@@ -0,0 +1,34 @@
+import turtle
+turtle.pensize(8)
+
+turtle.color('blue')
+turtle.penup()
+turtle.goto(-118,-25)
+turtle.pendown()
+turtle.circle(58)
+
+turtle.color('black')
+turtle.penup()
+turtle.goto(0,-25)
+turtle.pendown()
+turtle.circle(58)
+
+turtle.color('red')
+turtle.penup()
+turtle.goto(110,-25)
+turtle.pendown()
+turtle.circle(58)
+
+turtle.color('yellow')
+turtle.penup()
+turtle.goto(-55, -75)
+turtle.pendown()
+turtle.circle(58)
+
+turtle.color('green')
+turtle.penup()
+turtle.goto(55 , -75)
+turtle.pendown()
+turtle.circle(58)
+
+turtle.done()
